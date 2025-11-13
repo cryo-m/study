0a000da7604711d45891ea53ca623eb92350faab
Ilya <oparnikovillya@gmail.com>
2025-11-13 23:14:47 +0200

    added lab5 code

---
+++ lab5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    int sum = 0;
+    int mult = 1;
+    float Y, Z, x, y;
+    cout << "Введіть значення x та y: ";
+    cin >> x >> y;
+    for (int n = 1; n <= 5; n++) {
+        sum += cos(n);
+    }
+    Y = (tan(x * x) + sum) / (x + 5);
+    for (int k = 1; k <= 10; k++) {
+        mult *= log(k * y);
+    }
+    Z = mult + y * y;
+    printf("Y = %f\nZ = %f\n", Y, Z);
+}
